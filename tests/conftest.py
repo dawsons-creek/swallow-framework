4c75724297f210625eb7d8135df7d24137293b95
Alastair Dawson <283834+kreek@users.noreply.github.com>
2025-03-13 14:28:57 -0700

    Remove unused import.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ Configuration for pytest.
 
 import sys
 import os
-import pytest
 
 # Make sure the package is importable during tests
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
